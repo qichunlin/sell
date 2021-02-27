e98faedcc1fdaedaeeab23305267aedd56da9bff
qichunlin <737795279@qq.com>
2021-02-28 00:16:58 +0800

    [UPDATE] mybatis通过xml方式复习

--- src/main/java/com/legend/sell/mapper/ProductCategoryMapper.java
+++ src/main/java/com/legend/sell/mapper/ProductCategoryMapper.java
@@ -3,6 +3,7 @@ package com.legend.sell.mapper;
 import com.legend.sell.entity.ProductCategory;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +16,39 @@ import java.util.Map;
  */
 @Mapper
 public interface ProductCategoryMapper {
+
+    /**
+     * 插入数据
+     *
+     * @return
+     */
+    int insertProductCategoryByXml(ProductCategory productCategory);
+
+    /**
+     * 获取所有数据
+     *
+     * @return
+     */
+    List<ProductCategory> findAllByXml();
+
+    /**
+     * 删除数据
+     *
+     * @param categoryId
+     * @return
+     */
+    @Delete("DELETE FROM `weixin_sell`.`product_category` WHERE `category_id`= #{categoryId, jdbcType=INTEGER}")
+    int deleteProductCategory(@Param("categoryId") Integer categoryId);
+
+    /**
+     * 更新数据
+     *
+     * @param categoryId
+     * @param categoryName
+     * @return
+     */
+    @Update("UPDATE `weixin_sell`.`product_category` SET `category_name` = #{categoryName, jdbcType=VARCHAR} WHERE `category_id`= #{categoryId, jdbcType=INTEGER}")
+    int updateByProductCategory(@Param("categoryId") Integer categoryId, @Param("categoryName") String categoryName);
 
     /**
      * 查询数据
@@ -32,6 +66,24 @@ public interface ProductCategoryMapper {
             @Result(column = "update_time", property = "updateTime")
     })
     ProductCategory findProductCategory(Integer categoryType);
+
+
+    /**
+     * 查询数据
+     * Results注解是为了将结果映射回去对象 不然对象里面都是null
+     *
+     * @param categoryName
+     * @return
+     */
+    @Select("select * from `product_category` where category_name=#{categoryName}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_type", property = "categoryType"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime")
+    })
+    List<ProductCategory> findProductCategoryByName(String categoryName);
 
     /**
      * 插入数据通过对象
--- src/test/java/com/legend/sell/service/impl/ProductCategoryMapperTest.java
+++ src/test/java/com/legend/sell/service/impl/ProductCategoryMapperTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -48,7 +49,39 @@ public class ProductCategoryMapperTest {
     @Test
     public void findProductCategoryTest() {
         ProductCategory productCategory = productCategoryMapper.findProductCategory(11);
-        log.info("dddd:{}", productCategory);
+        log.info("查询商品分类通过类型:{}", productCategory);
+    }
+
+    @Test
+    public void findProductCategoryByNameTest() {
+        List<ProductCategory> productCategory = productCategoryMapper.findProductCategoryByName("2222222");
+        log.info("查询商品分类通过名字:{}", productCategory);
+    }
+
+    @Test
+    public void updateByProductCategoryTest() {
+        int result = productCategoryMapper.updateByProductCategory(1,"2222222");
+        log.info("更新商品分类通过id:{}", result);
+    }
+
+    @Test
+    public void deleteProductCategoryTest() {
+        int result = productCategoryMapper.deleteProductCategory(1);
+        log.info("删除商品分类通过id:{}", result);
+    }
+
+    @Test
+    public void findAllByXmlTest() {
+        List<ProductCategory> productCategory = productCategoryMapper.findAllByXml();
+        log.info("查询所有商品分类:{}", productCategory);
+    }
+
+    @Test
+    public void insertProductCategoryByXmlTest() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryType(90);
+        productCategory.setCategoryName("测试xml");
+        productCategoryMapper.insertProductCategoryByXml(productCategory);
     }
 
 }
